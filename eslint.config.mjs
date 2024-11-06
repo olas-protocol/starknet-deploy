a2be3dc4dc5e6d83e0d316f67bbb5cbc8a212dc0
Rojhat Toptamus <rojhattoptamus@gmail.com>
2024-11-07 02:11:40 +0300

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,9 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
-);
+export default tseslint.config({
+  // Extend ESLint's recommended rules
+  extends: [eslint.configs.recommended, ...tseslint.configs.recommended],
+  // Ignore patterns
+  ignores: ['node_modules/**', 'dist/**'],
+});
