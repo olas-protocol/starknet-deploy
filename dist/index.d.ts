fa70c2e8d1877258e2fea25ff590251e797d71d2
Suraj Kohli <kohlisuraj94@gmail.com>
2025-01-29 16:36:24 -0500

    build files using tsup

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+declare function helloStarknet(): void;
+
+export { helloStarknet };
---
+++ dist/index.mjs
@@ -0,0 +1,7 @@
+// src/index.ts
+function helloStarknet() {
+  console.log("Hello Starknet!");
+}
+export {
+  helloStarknet
+};
